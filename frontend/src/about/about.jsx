0e24da7fa8fed06897f839b420cbc48f4aea350d
luan-prates <pratesr21@gmail.com>
2017-03-10 19:03:22 -0300

    Corpo da pagina estática

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -4,5 +4,12 @@ import PageHeader from '../template/pageHeader'
 export default props => (
     <div>
         <PageHeader name='Sobre' small='Nós'></PageHeader>
+        
+        <h2>Nossa História</h2>
+        <p>Somos uma empresa de desenvolvimento de softwares de alta qualidade, focada na entrega de valor para os nossos clientes.</p>
+        <h2>Missão e Visão</h2>
+        <p>Entregar as melhores soluções para nossos clientes</p>
+        <h2>Imprensa</h2>
+        <p>Usamos fortemente as redes sociais</p>
     </div>    
 )
