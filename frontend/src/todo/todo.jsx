2fff7c0a599c8c0924da1ce928a1baf955dc3b08
luan-prates <pratesr21@gmail.com>
2017-03-22 09:41:15 -0300

    Garantindo que o this é o this. rsrs
    No JavaScript o this muda de acordo com quem está fazendo a chamada.
    Estamos garantindo que isso não aconteça.

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,6 +4,10 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
 
     handleAdd(){
         console.log(this)
